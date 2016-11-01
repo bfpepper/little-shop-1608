d3d5c0749e6e63a61540d9600bae49bf968a3d10
antciccone <aciccone@bu.edu>
2016-10-31 19:01:50 -0600

    visitor can view trips by category

---
+++ spec/features/categories/a_visitor_can_see_all_trips_by_category_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+feature "visitor visits category show page" do
+  scenario "visitor can view all trips in that category" do
+    trip1, trip2 = create_list(:trip_with_category, 2)
+    category = trip1.category
+    trip3 = category.trips.create(title: "test", description: "test", price: 1.23, image_url: "url")
+    visit category_path(category)
+
+    expect(page).to have_content trip1.title
+    expect(page).to have_content trip3.title
+  end
+end
