5e69e08069edf368b4fd0e30e4a9c7fca91496e0
Ryan Travitz <rtravitz@gmail.com>
2016-11-01 11:20:25 -0600

    Change categories route to use the name near the root.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,8 @@
 Trip.destroy_all
 Category.destroy_all
 
-hot = Category.create(title: "Hot")
-cold = Category.create(title: "Cold")
+hot = Category.create(title: "hot")
+cold = Category.create(title: "cold")
 
 hot.trips.create(title: "Jungle", description: "Eat bugs!", image_url: "http://www.theimaginativeconservative.org/wp-content/uploads/2016/06/the-jungle.jpg", price: 500)
 cold.trips.create(title: "Antarctica", description: "Freeze your nuts off!", image_url: "http://getworldmedia.com/wp-content/uploads/2016/01/antarctica-factsAntarctica2.jpg", price: 1000)
