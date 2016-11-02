b6df4800d7a6cc4b26e678508a06708102818b42
Ryan Travitz <rtravitz@gmail.com>
2016-11-02 15:13:32 -0600

    Add status to orders

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161102210326) do
+ActiveRecord::Schema.define(version: 20161102211119) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,8 +22,9 @@ ActiveRecord::Schema.define(version: 20161102210326) do
   create_table "orders", force: :cascade do |t|
     t.float    "cost"
     t.integer  "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",                     null: false
+    t.datetime "updated_at",                     null: false
+    t.string   "status",     default: "ordered"
     t.index ["user_id"], name: "index_orders_on_user_id", using: :btree
   end
 
