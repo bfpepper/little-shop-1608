bd7e991c6decf4d99cda8b93048fd7660c039413
antciccone <aciccone@bu.edu>
2016-11-05 14:57:48 -0600

    can filter ordered by status

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,7 +1,11 @@
 class Admin::DashboardController < Admin::BaseController
 
   def index
-    @orders = Order.all.order(created_at: :desc)
+    if params[:status]
+      @orders = Order.where(status: params[:status]).order(created_at: :desc)
+    else
+      @orders = Order.all.order(created_at: :desc)
+    end
   end
 
   def show
