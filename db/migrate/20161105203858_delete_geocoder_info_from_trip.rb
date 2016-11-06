9e5a7412233ed75a169359a0e90144e9b9bfa6b3
Ben Pepper <bfpepper87@gmail.com>
2016-11-06 16:42:56 -0700

    fixed migration issue.

--- db/migrate/20161105203858_delete_geocoder_info_from_trip.rb
+++
@@ -1,7 +0,0 @@
-class DeleteGeocoderInfoFromTrip < ActiveRecord::Migration[5.0]
-  def change
-    remove_column :trips, :latitude
-    remove_column :trips, :longitude
-    remove_column :trips, :address
-  end
-end
