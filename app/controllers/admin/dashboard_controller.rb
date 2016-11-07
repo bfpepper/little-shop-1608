8b30fe6a097094821b96dc328765e161129416ba
Joey Stansfield <joseph.r.stansfield@gmail.com>
2016-11-07 15:14:58 -0700

    halfway there

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,10 +1,17 @@
 class Admin::DashboardController < Admin::BaseController
 
   def index
+    @orders = Order.all.order(created_at: :desc)
+  end
+
+  def change_status
     if params[:status]
       @orders = Order.where(status: params[:status]).order(created_at: :desc)
     else
       @orders = Order.all.order(created_at: :desc)
+    end
+    respond_to do |format|
+      format.js
     end
   end
 
