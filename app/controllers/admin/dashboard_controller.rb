3f67282a69b29f8411a640b544741ff5bb5f0680
Joey Stansfield <joseph.r.stansfield@gmail.com>
2016-11-07 15:50:14 -0700

    smoothly change between order status in admin dashboard

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -6,7 +6,8 @@ class Admin::DashboardController < Admin::BaseController
 
   def change_status
     if params[:status]
-      @orders = Order.where(status: params[:status]).order(created_at: :desc)
+      @orders = Order.all.order(created_at: :desc) if params[:status] == "all orders"
+      @orders = Order.where(status: params[:status]).order(created_at: :desc) unless params[:status] == "all orders"
     else
       @orders = Order.all.order(created_at: :desc)
     end
