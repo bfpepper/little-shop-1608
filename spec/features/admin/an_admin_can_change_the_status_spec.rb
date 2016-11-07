c59107013391e54617792d52965a6127140995c0
antciccone <aciccone@bu.edu>
2016-11-07 14:22:11 -0700

    button disabled if order status is not paid

--- spec/features/admin/an_admin_can_change_the_status_spec.rb
+++ spec/features/admin/an_admin_can_change_the_status_spec.rb
@@ -98,4 +98,22 @@ describe 'an admin visists /dashboard' do
 
       expect(page).to have_button('Mark Paid', disabled: true)
   end
-end 
+
+  scenario 'an admin can only mark as paid if order status is ordered' do
+    trip1, trip2 = create_list(:trip, 22)
+
+    admin_user = create(:user)
+    admin_user.admin!
+    user1, user2 = create_list(:user, 2)
+    user1.orders.create(cost: 12, status: "completed")
+    user2.orders.create(cost: 12, status: "cancelled")
+    user1.orders.first.orders_trips.create(trip_id: trip1.id, quantity: 10, trip_price: trip1.price)
+    user2.orders.first.orders_trips.create(trip_id: trip2.id, quantity: 10, trip_price: trip2.price)
+
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin_user)
+
+    visit admin_dashboard_path
+
+    expect(page).to have_button('Mark Completed', disabled: true)
+  end
+end
